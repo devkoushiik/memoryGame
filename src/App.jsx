892220bd9127a9d77d431ed307a181907551a0a4
koushik ahmed <koushikxahmed@gmail.com>
2025-02-19 15:34:09 +0600

    data fetching done using emojihub api

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,22 @@ import MemoryCard from "./components/MemoryCard";
 export default function App() {
   const [isGameOn, setIsGameOn] = useState(false);
 
-  function startGame(e) {
+  async function startGame(e) {
     e.preventDefault();
-    setIsGameOn(true);
+
+    try {
+      const response = await fetch(
+        "https://emojihub.yurace.pro/api/all/category/animals-and-nature"
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log(data);
+      setIsGameOn(true);
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
 
   function turnCard() {
