c78723921c0b2455ea0ffec15bbe7285bd94d833
koushik ahmed <koushikxahmed@gmail.com>
2025-02-22 16:42:15 +0600

    keyboard navigation added

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,8 +1,13 @@
+import { useEffect, useRef } from "react";
 import RegularButton from "./RegularButton";
 
 const GameOver = ({ resetGameHandler }) => {
+  const divRef = useRef(null);
+  useEffect(() => {
+    divRef.current.focus();
+  }, []);
   return (
-    <div className="wrapper wrapper--accent">
+    <div className="wrapper wrapper--accent" ref={divRef} tabIndex="-1">
       <p className="p--large">You have matched all memory cards!!</p>
       <RegularButton handleClick={resetGameHandler}>
         Restart the game again!!
