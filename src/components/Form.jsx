794e114b75bcf91657d8c9516f217034ccfc54e6
koushik ahmed <koushikxahmed@gmail.com>
2025-02-23 14:45:05 +0600

    label refactor done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,14 +5,10 @@ export default function Form({ handleSubmit, handleChange }) {
   return (
     <div className="form-container">
       <form className="wrapper">
-        <div className="form__inner-wrapper">
-          <label htmlFor="category">Select emoji category</label>
-          <SelectElement handleChange={handleChange} target={"category"} />
-        </div>
-        <div className="form__inner-wrapper">
-          <label htmlFor="number">Select numbers of memory cards.</label>
-          <SelectElement handleChange={handleChange} target={"number"} />
-        </div>
+        <SelectElement handleChange={handleChange} target={"category"} />
+
+        <SelectElement handleChange={handleChange} target={"number"} />
+
         <RegularButton handleClick={handleSubmit}>Start Game</RegularButton>
       </form>
     </div>
--- src/components/SelectElement.jsx
+++ src/components/SelectElement.jsx
@@ -3,21 +3,29 @@ import { data } from "../data/data";
 const SelectElement = ({ handleChange, target }) => {
   console.log(target);
   return (
-    <select name={target} id={target} onChange={handleChange}>
-      {target === "category"
-        ? data[target].map((item, index) => (
-            <option key={index} value={item.value}>
-              {item.name}
-            </option>
-          ))
-        : target === "number"
-        ? data[target].map((item, index) => (
-            <option key={index} value={item.value}>
-              {item.value}
-            </option>
-          ))
-        : null}
-    </select>
+    <div className="form__inner-wrapper">
+      {target === "category" ? (
+        <label htmlFor={target}>Select emoji {target}</label>
+      ) : null}
+      {target === "number" ? (
+        <label htmlFor={target}>Select numbers of memory cards.</label>
+      ) : null}
+      <select name={target} id={target} onChange={handleChange}>
+        {target === "category"
+          ? data[target].map((item, index) => (
+              <option key={index} value={item.value}>
+                {item.name}
+              </option>
+            ))
+          : target === "number"
+          ? data[target].map((item, index) => (
+              <option key={index} value={item.value}>
+                {item.value}
+              </option>
+            ))
+          : null}
+      </select>
+    </div>
   );
 };
 export default SelectElement;
